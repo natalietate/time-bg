d78a61d7683eb08df3cb868a0ead233096725795
natalietate <martin.nat@gmail.com>
2017-11-22 11:01:32 -0500

    clean up js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ setInterval(logTime, 1000); // check time every second
   if (hrs >= 4 && hrs <= 7) {
     document.body.className = "earlyAm"; // change to this background gradient
     greeting.innerHTML = "Wow, you're up early!"; // add this greeting
-    quote.innerHTML = "'I believe great people do things before they are ready.' - Amy Poehler" // add this quote
+    quote.innerHTML = "'I believe great people do things before they are ready.' - Amy Poehler"; // add this quote
   }
   // morning
   else if (hrs >= 8 && hrs <= 11) {
@@ -53,26 +53,26 @@ setInterval(logTime, 1000); // check time every second
   else if (hrs >= 12 && hrs <= 15) {
     document.body.className = "afternoon";
     greeting.innerHTML = "It's probably time for a break";
-    quote.innerHTML = "'It is possible to commit no mistakes and still lose.' - Captain Jean-Luc Picard"
+    quote.innerHTML = "'It is possible to commit no mistakes and still lose.' - Captain Jean-Luc Picard";
   }
   // evening
   else if (hrs >= 16 && hrs <= 19) {
-    document.body.className = "evening",
+    document.body.className = "evening";
       greeting.innerHTML = "Is it time for happy hour yet?";
-    quote.innerHTML = "'Friends should be like books, few, but hand-selected.' ― C.J. Langenhoven"
+    quote.innerHTML = "'Friends should be like books, few, but hand-selected.' ― C.J. Langenhoven";
   }
   // night
   else if (hrs >= 20 && hrs <= 23) {
     document.body.className = "night";
     greeting.innerHTML = "Have a great night.";
-    quote.innerHTML = "'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.' - Douglas Adams"
+    quote.innerHTML = "'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.' - Douglas Adams";
   }
   // late night
   else {
-    document.body.className = "lateNight"
+    document.body.className = "lateNight";
     greeting.innerHTML = "You're up late!";
-    quote.innerHTML = "'The good thing about science is that it's true whether or not you believe in it.' - Neil deGrasse Tyson "
-  };
+    quote.innerHTML = "'The good thing about science is that it's true whether or not you believe in it.' - Neil deGrasse Tyson";
+  }
   setTimeout(logHour, 60000); // check time every minute, in case someone is viewing page at XX:59
 })();
 
